0cb1eaf3c9ebb6f867fd708259ff5e365d53020e
fabio4520 <fabioleofc@gmail.com>
2022-04-30 12:02:15 -0500

    ADD: Generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # resources :involved_companies
   resources :users
   resources :genres
@@ -15,5 +16,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "games#index"
 end
