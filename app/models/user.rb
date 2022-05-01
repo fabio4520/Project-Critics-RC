8ac742479c8439c5263286155a0c34326730ba13
fabio4520 <fabioleofc@gmail.com>
2022-05-01 08:36:29 -0500

    Fixing all bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :trackable
+         :recoverable, :rememberable, :validatable
+        #  :confirmable, :lockable, :trackable
   has_one_attached :avatar
   has_many :critics, dependent: :destroy
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_30_165618) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_30_212512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,10 +111,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_30_165618) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.integer "role"
+    t.string "role"
     t.integer "critics_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -131,14 +132,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_30_165618) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "critics", "users"
   add_foreign_key "games", "games", column: "parent_id"
+  add_foreign_key "involved_companies", "companies"
+  add_foreign_key "involved_companies", "games"
 end
