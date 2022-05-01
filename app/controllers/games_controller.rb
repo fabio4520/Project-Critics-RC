35dc65999df3bd1166329c5c8d1fb2819b26a6a2
Diego Manrique <diego.manrique.m@uni.pe>
2022-04-30 23:25:21 -0500

    Add functionality game show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: %i[ show edit update destroy ]
+  before_action :set_game, only: %i[ show edit update destroy add_genre add_platform add_developer add_publisher remove_genre remove_platform remove_developer remove_publisher]
 
   # GET /games
   def index
@@ -8,7 +8,59 @@ class GamesController < ApplicationController
 
   # GET /games/1
   def show
+    # @genres = Genre.all
+    # @platforms = Platform.all
   end
+ 
+  # PATCH /games/:id/add-genre
+  def add_genre
+    genre = Genre.find(params[:genre_id])
+    @game.genres << genre
+    redirect_to @game
+  end
+
+  def add_platform
+    platform = Platform.find(params[:platform_id])
+    @game.platforms << platform
+    redirect_to @game
+  end
+
+  def add_developer
+    company = Company.find(params[:company_id])
+    @game.involved_companies.create(developer: true, publisher: false, company: company)
+    redirect_to @game
+  end
+
+  def add_publisher
+    company = Company.find(params[:company_id])
+    @game.involved_companies.create(developer: false, publisher: true, company: company)
+    redirect_to @game
+  end
+
+  def remove_genre
+    genre = Genre.find(params[:genre_id])
+    @game.genres.delete(genre)
+    redirect_to games_path
+  end
+
+  def remove_platform
+    platform = Platform.find(params[:platform_id])
+    @game.platforms.delete(platform)
+    redirect_to games_path
+  end
+
+  def remove_developer
+    company = InvolvedCompany.find(params[:company_id])
+    @game.involved_companies.delete(company)
+    redirect_to games_path
+  end
+
+  def remove_publisher
+    company = InvolvedCompany.find(params[:company_id])
+    @game.involved_companies.delete(company)
+    redirect_to games_path
+  end
+
 
   # GET /games/new
   def new
@@ -39,6 +91,7 @@ class GamesController < ApplicationController
     end
   end
 
+
   # DELETE /games/1
   def destroy
     @game.destroy
@@ -54,5 +107,17 @@ class GamesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def game_params
       params.require(:game).permit(:name, :summary, :release_date, :category, :rating, :cover)
+    end
+
+    def genre_params
+      params.require(:genre).permit(:id, :name)
+    end
+
+    def  platform_params
+      params.require(:platform).permit(:id, :name)
+    end
+
+    def involved_companies_params
+      params.require(:involved_company).permit(:id, :name)
     end
 end
